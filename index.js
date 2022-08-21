091cae28cd611dccc0e2e4f883b9b985891779b7
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2022-08-21 22:58:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * This code is deployed as the Lambda function in AWS. 
- * It just returns the simple string Hello World.
+ * It just returns the input event as it is
  */
 
 exports.handler = async(event, context) => {
